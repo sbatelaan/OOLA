a3ead6ce47767a673b1bd8c1b9dcf4f102800cdb
stephen Batelaan <sbatelaan91@gmail.com>
2023-07-30 12:04:12 -0700

    finsihed

--- Profile.cs
+++ Profile.cs
@@ -2,4 +2,55 @@ using System;
 
 namespace DatingProfile
 { 
+    class Profile
+    {
+        //fields (attributes) of the Profile class
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+
+            // Constructor to create a Profile object with basic information
+        public Profile( string name, int age, string city, string country, string pronouns = "they/them")
+        {
+
+            // Assigning the values passed as arguments to the respective fields
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+
+            // Initializing the 'hobbies' array with an empty array
+            this.hobbies = new string[0];
+        }
+
+        //Method to view the profile information as a formatted string
+        public string ViewProfile()
+        {
+            // creating a string to store the information
+            string bio = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}";
+
+                //Adding the hobbies information to the profile
+            bio += "\nHobbies:\n";
+            foreach (string hobby in hobbies)
+            {
+                bio += $"- {hobby}\n";
+
+            }
+
+            // Returning the complete profile information as a formatted string
+            return bio;
+        }
+
+            //Method to set the hobbies for the profile
+        public void SetHobbies(string[] hobbies)
+        {
+            //Assigning the provided hgobbies array to the 'hobbies' field
+            this.hobbies = hobbies;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace DatingProfile
   {
     static void Main(string[] args)
     {
-      
+        // Creating a new profile object named 'sam' with the provided information
+      Profile sam = new Profile("Sam Drakkila", 30, "NewYork", "USA", "he/him");
+      // setting the hobbies for the 'sam' profile using an array of strings
+      sam.SetHobbies(new string[] {
+        "listening to audiobooks and podcasts",
+        "playing rec sports like bowling and kickball",
+        "writing a speculative fiction novel",
+        "reading advice columns"
+      });
+
+      Console.WriteLine(sam.ViewProfile());
+        
     }
   }
 }
